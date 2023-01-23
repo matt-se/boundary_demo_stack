7d3d634a2b56d18249b7ff591c05b607a049cc22
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-23 09:52:57 -0500

    Update worker_instance.tf

--- worker_instance.tf
+++ worker_instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   ami           = var.worker_ami
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key_for_ssh_acccess_to_worker.key_name
-  subnet_id     = var.subnet_id
+  subnet_id     = aws_subnet.subnet_public.id
   vpc_security_group_ids      = [aws_security_group.sg_worker.id]
   associate_public_ip_address = true
 
