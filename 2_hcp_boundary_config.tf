e04f7df474ffa9c873d263c7aca9f2be9d6e6665
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-23 15:00:23 -0500

    Update 2_hcp_boundary_config.tf

--- 2_hcp_boundary_config.tf
+++ 2_hcp_boundary_config.tf
@@ -27,9 +27,6 @@ resource "boundary_host_catalog_static" "us_east_1_dev" {
 
 
 resource "boundary_host_static" "web_server" {
-  depends_on = [
-    aws_instance.web
-  ]
   name            = "${var.app_prefix}_web_${var.environment}"
   description     = "frontend web server for ${var.app_prefix} in ${var.environment}"
   address         = aws_instance.web.public_ip
@@ -44,6 +41,9 @@ resource "boundary_host_set_static" "web_servers" {
   host_catalog_id = boundary_host_catalog_static.us_east_1_dev.id
   host_ids = [
     boundary_host_static.web_server.id
+  ]
+  depends_on = [
+    aws_instance.web
   ]
 }
 
