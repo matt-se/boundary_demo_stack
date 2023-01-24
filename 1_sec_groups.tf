b6c03b79121f0ddedc0fa18fba5dbd532903f97e
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-24 15:29:57 -0500

    network

--- 1_sec_groups.tf
+++ 1_sec_groups.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "sg_worker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    from_port   = 9202
+    to_port     = 9202
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
---
+++ boundary_setup.sh
@@ -0,0 +1,8 @@
+mkdir /home/ubuntu/boundary/ && cd /home/ubuntu/boundary/
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install boundary-worker-hcp -y
+sudo touch /home/ubuntu/boundary/pki-worker.hcl
+
+printf "foo" | sudo tee pki-worker.hcl
+boundary-worker server -config="/home/ubuntu/boundary/pki-worker.hcl"
