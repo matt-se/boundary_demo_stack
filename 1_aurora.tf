1222c5e141efd769a6559e4ff9cc508f839aba67
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 12:11:56 -0500

    Update 1_aurora.tf

--- 1_aurora.tf
+++ 1_aurora.tf
@@ -1,6 +1,7 @@
 resource "aws_db_instance" "db" {
   allocated_storage    = 10
   db_name              = "mydb"
+  identifier = "${var.app_prefix}_db_${var.environment}"
   engine               = "postgres"
   #engine_version       = "5.7"
   instance_class       = "db.t3.micro"
@@ -8,4 +9,5 @@ resource "aws_db_instance" "db" {
   password             = "foobarbaz"
   #parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  db_subnet_group_name = aws_subnet.subnet_public.id
 }
