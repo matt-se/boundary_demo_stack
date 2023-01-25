ef2d13ec5e357bc0a3ac54fccef282fe16eb2516
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 13:15:18 -0500

    Update 1_aurora.tf

--- 1_aurora.tf
+++ 1_aurora.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "subnet" {
   name       = "${var.app_prefix}_db_subnet_${var.environment}"
-  subnet_ids = [aws_vpc.vpc.subnet_ids ]
+  subnet_ids = [aws_vpc.vpc.subnet_id]
 }
 
 
