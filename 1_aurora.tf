c709138bf3e71973f48c86a866786c2e25616f02
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 11:52:22 -0500

    Create 1_aurora.tf

---
+++ 1_aurora.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "db" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "postgres"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
