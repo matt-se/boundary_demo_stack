1c16e7a21a5d5b1667c3ea07df0a00c485df58c4
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 13:19:01 -0500

    Update 1_networking.tf

--- 1_networking.tf
+++ 1_networking.tf
@@ -9,15 +9,20 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.app_prefix}_igw_${var.environment}"
+  }
 }
 
 resource "aws_subnet" "subnet_public" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.cidr_subnet
+  name = "${var.app_prefix}_subnet_public_${var.environment}"
 }
 
 resource "aws_route_table" "rtb_public" {
   vpc_id = aws_vpc.vpc.id
+  name = "${var.app_prefix}_rtb_public_${var.environment}"
 
   route {
     cidr_block = "0.0.0.0/0"
