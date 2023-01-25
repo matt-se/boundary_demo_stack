23f5b89378fe2f9fdcbb0b78a5207d22e1897d18
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 13:42:39 -0500

    Update 1_main_variables.tf

--- 1_main_variables.tf
+++ 1_main_variables.tf
@@ -65,7 +65,11 @@ variable "cidr_vpc" {
 }
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
-  default     = "10.1.0.0/24"
+  default     = "10.1.1.0/24"
+}
+variable "cidr_subnet2" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.2.0/24"
 }
 
 
