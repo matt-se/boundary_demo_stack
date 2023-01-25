0fee0d92a58d72f163a05a41d9e6b69eb6434532
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-25 12:14:30 -0500

    Update 1_aurora.tf

--- 1_aurora.tf
+++ 1_aurora.tf
@@ -1,7 +1,13 @@
+resource "aws_db_subnet_group" "subnet" {
+  name       = "${var.app_prefix}_db_subnet_${var.environment}"
+  subnet_ids = aws_vpc.vpc.subnet_ids
+}
+
+
 resource "aws_db_instance" "db" {
   allocated_storage    = 10
   db_name              = "mydb"
-  identifier = "${var.app_prefix}_db_${var.environment}"
+  identifier           = "${var.app_prefix}_db_${var.environment}"
   engine               = "postgres"
   #engine_version       = "5.7"
   instance_class       = "db.t3.micro"
@@ -9,5 +15,5 @@ resource "aws_db_instance" "db" {
   password             = "foobarbaz"
   #parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  db_subnet_group_name = aws_subnet.subnet_public.id
+  db_subnet_group_name = aws_db_subnet_group.subnet.name
 }
