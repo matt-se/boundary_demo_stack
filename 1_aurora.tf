afadefca0969a1fa1120c6329d46e778cc2d7ae0
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-26 11:30:58 -0500

    sec groups for aurora

--- 1_aurora.tf
+++ 1_aurora.tf
@@ -14,4 +14,5 @@ resource "aws_db_instance" "db" {
   password             = var.aws_rds_password
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.subnet.name
+  vpc_security_group_ids = [aws_security_group.sg_db.id]
 }
