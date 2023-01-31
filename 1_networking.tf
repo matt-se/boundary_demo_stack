d682a73ca18e2bc3c5c5bee6d3223dc3bd56484e
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-01-31 11:03:25 -0500

    Update 1_networking.tf

--- 1_networking.tf
+++ 1_networking.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "subnet_public" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.cidr_subnet
-  availability_zone = "us-east-1c"
+  availability_zone = "us-east-2c"
   tags = {
     Name = "${var.app_prefix}_subnet_public_${var.environment}"
   }
@@ -26,7 +26,7 @@ resource "aws_subnet" "subnet_public" {
 resource "aws_subnet" "subnet_public2" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.cidr_subnet2
-  availability_zone = "us-east-1d"
+  availability_zone = "us-east-2d"
   tags = {
     Name = "${var.app_prefix}_subnet_public_2_${var.environment}"
   }
