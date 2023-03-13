c07dc5f5e13ed563ee2eac59c1038046a2a1f597
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-03-13 13:15:12 -0400

    cidr

--- 1_main_variables.tf
+++ 1_main_variables.tf
@@ -61,7 +61,7 @@ variable "boundary_lower_user_password" {
 ##########  AWS networking
 variable "cidr_vpc" {
   description = "CIDR block for the VPC"
-  default     = "10.0.0.0/8"
+  default     = "10.0.0.0/16"
 }
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
