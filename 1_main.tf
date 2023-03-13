8d846cdef35e53d261ddd58dcc9564d87604399a
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-03-13 13:53:38 -0400

    Update 1_main.tf

--- 1_main.tf
+++ 1_main.tf
@@ -30,6 +30,6 @@ provider "boundary" {
 
 #provider "vault" {}
 
-data "vault_generic_secret" "keys" {
-  path = "secrets/aws-keys/key"
-}
+#data "vault_generic_secret" "keys" {
+#  path = "secrets/aws-keys/key"
+#}
