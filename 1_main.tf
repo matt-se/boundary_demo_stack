831c3554ba3b27353812830a6298a890ba9743c6
matt-se <101576447+matt-se@users.noreply.github.com>
2023-03-15 10:08:15 -0500

    Update 1_main.tf

--- 1_main.tf
+++ 1_main.tf
@@ -33,10 +33,11 @@ provider "vault" {}
 
 
 
-
+/*
 data "vault_generic_secret" "keys" {
   path = "secret/aws"
 }
 output "mypassword" {
  value = data.vault_generic_secret.keys.data["public"]
 }
+*/
