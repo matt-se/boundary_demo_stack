075295668c5dc6d38be9aa41d173714fdb54b11d
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-05-05 13:49:39 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ listener "tcp" {
 }
         
 worker {
-  public_addr = "${public_ip}"
+  public_addr = "${curl 'http://checkip.amazonaws.com'}"
   controller_generated_activation_token = "${controller_generated_activation_token}"
   auth_storage_path = "home/ubuntu/boundary/worker1"
   tags {
