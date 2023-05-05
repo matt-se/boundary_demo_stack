807999bbbe7ee6920d7492d63668f7e74c3f82c5
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-05-05 09:40:52 -0400

    update user_data

--- 1_worker_instance.tf
+++ 1_worker_instance.tf
@@ -23,34 +23,32 @@ resource "aws_instance" "boundary_worker" {
     version = var.app_version
   }
   user_data_replace_on_change = true
-  user_data = "${file("script.txt")}"
-  /*
-  user_data   = <<EOF
-mkdir /home/ubuntu/boundary/ && cd /home/ubuntu/boundary/
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install boundary-worker-hcp -y
-sudo touch /home/ubuntu/boundary/pki-worker.hcl
+  #user_data = "${file("script.txt")}"
+  
+  user_data   = -<<EOF
+        mkdir /home/ubuntu/boundary/ && cd /home/ubuntu/boundary/
+        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+        sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+        sudo apt-get update && sudo apt-get install boundary-worker-hcp -y
+        sudo touch /home/ubuntu/boundary/pki-worker.hcl
 
-printf "disable_mlock = true
-hcp_boundary_cluster_id = "${var.boundary_cluster_id}"
-listener "tcp" {
-  address = "0.0.0.0:9202"
-  purpose = "proxy"
-}
-        
-worker {
-  public_addr = "44.201.8.40"
-  controller_generated_activation_token = "${boundary_worker.worker.controller_generated_activation_token}"
-  auth_storage_path = "home/ubuntu/boundary/worker1"
-  tags {
-    type = ["worker"]
-  }
-}" | sudo tee pki-worker.hcl
-boundary-worker server -config="/home/ubuntu/boundary/pki-worker.hcl"
-EOF
-*/
-
+        printf "disable_mlock = true
+        hcp_boundary_cluster_id = "${var.boundary_cluster_id}"
+        listener "tcp" {
+          address = "0.0.0.0:9202"
+          purpose = "proxy"
+        }
+                
+        worker {
+          public_addr = "44.201.8.40"
+          controller_generated_activation_token = "${boundary_worker.worker.controller_generated_activation_token}"
+          auth_storage_path = "home/ubuntu/boundary/worker1"
+          tags {
+            type = ["worker"]
+          }
+        }" | sudo tee pki-worker.hcl
+        boundary-worker server -config="/home/ubuntu/boundary/pki-worker.hcl"
+        EOF
 
   
   connection {
--- boundary-worker.hcl
+++ boundary-worker.hcl
@@ -6,8 +6,8 @@ listener "tcp" {
 }
         
 worker {
-  public_addr = "44.201.8.40"
-  controller_generated_activation_token = "neslat_2Kq91Y3xyS1j5U9Ycj21rZfFg2raKge32N46BLMvrASi3E5wAiFNzGi9EivuNCYpdDWUqX6irYCKVjugfJQU42JNMMQcZ"
+  public_addr = "18.209.241.150"
+  controller_generated_activation_token = "neslat_2KrkFMrCtM8GnTrzvjTQrWBfGB5Q7wdM6Eea53vhyFmzacG79m8kDYziwQ16cJ6jtqWhYsUta8k3MJnp5F5GwuybjwrJN"
   auth_storage_path = "home/ubuntu/boundary/worker1"
   tags {
     type = ["worker"]
