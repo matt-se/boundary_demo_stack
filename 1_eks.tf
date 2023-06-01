f19ee1a90ce8ff2fe1191c49cb0c61fe9085ab04
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-06-01 15:14:29 -0400

    Update 1_eks.tf

--- 1_eks.tf
+++ 1_eks.tf
@@ -1,36 +1,106 @@
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "${var.app_prefix}-eks-cluster-${var.environment}"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+resource "aws_iam_role" "eks-iam-role" {
+ name = "eks_iam_role_${var.app_prefix}_${var.environment}"
 
-  vpc_config {
-    subnet_ids         = [aws_subnet.subnet_public.id]
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
-  }
-}
+ path = "/"
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role_${var.app_prefix}_${var.environment}"
-
-  assume_role_policy = <<POLICY
+ assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
 }
-POLICY
+EOF
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
 }
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ role    = aws_iam_role.eks-iam-role.name
+}
+
+
+resource "aws_eks_cluster" "eks_cluster" {
+ name     = "${var.app_prefix}-eks-cluster-${var.environment}"
+ role_arn = aws_iam_role.eks-iam-role.arn
+ vpc_config {
+  subnet_ids = [aws_subnet.subnet_public.id, aws_subnet.subnet_public2.id]
+ }
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
+ security_group_ids = [aws_security_group.eks_cluster_sg.id]
+}
+
+
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+ 
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
+
+
+
+
+ resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks_workernodes_${var.environment}_${var.app_prefix}"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [aws_subnet.subnet_public.id, aws_subnet.subnet_public2.id]
+  instance_types = ["t2.micro"]
+ 
+  scaling_config {
+   desired_size = 3
+   max_size   = 3
+   min_size   = 1
+  }
+ 
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+ }
+
+
 
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks_cluster_sg_${var.app_prefix}_${var.environment}"
@@ -50,26 +120,6 @@ resource "aws_security_group" "eks_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group_${var.app_prefix}_${var.environment}"
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
-  }
-
-  remote_access {
-    ec2_ssh_key = "my-ec2-key"
-    source_security_group_ids = [aws_security_group.eks_cluster_sg.id]
-  }
-
-  subnet_ids = [aws_subnet.subnet_public.id]
-  node_role_arn = aws_iam_role.eks_cluster_role.arn
-}
-
 
 
 
