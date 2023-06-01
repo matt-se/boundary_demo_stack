cd8d6aa21d1987e55046d67d0318ebbe7c64e2cb
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-06-01 12:20:11 -0400

    ADD VAULT INSTANCE

--- 1_sec_groups.tf
+++ 1_sec_groups.tf
@@ -74,6 +74,36 @@ resource "aws_security_group" "sg_web_server" {
   }
 }
 
+
+resource "aws_security_group" "sg_vault_server" {
+  name   = "${var.app_prefix}_vault_serv_sg_${var.environment}"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg_worker.id]
+  }
+
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
 resource "aws_security_group" "sg_db" {
   name   = "${var.app_prefix}_db_sg_${var.environment}"
   vpc_id = aws_vpc.vpc.id
---
+++ 1_vault_instance.tf
@@ -0,0 +1,19 @@
+#resource "aws_key_pair" "key_for_ssh_acccess_to_web_server" {
+#  key_name   = "${var.app_prefix}_web_server_${var.environment}_keypair"
+#  public_key = var.web_server_public_key
+#}
+
+resource "aws_instance" "vault" {
+  ami           = var.vault_ami
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_for_ssh_acccess_to_web_server.key_name
+  subnet_id     = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_vault_server.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "${var.app_prefix}_vault_${var.environment}"
+    owner = var.owner
+    version = var.app_version
+  }
+}
