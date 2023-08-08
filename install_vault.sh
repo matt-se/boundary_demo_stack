c13c97b6a9ed58d557d4b5bb6999e95ed659b7d7
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-08-08 16:20:42 -0400

    Update install_vault.sh

--- install_vault.sh
+++ install_vault.sh
@@ -14,11 +14,13 @@ sudo rm -f vault_${VAULT_VERSION}_linux_amd64.zip
 # Set the VAULT_ADDR environment variable
 export VAULT_ADDR='http://127.0.0.1:8200'
 
-# Start Vault in dev mode with the root token set as 'matt'
-vault server -dev -dev-root-token-id="matt" -dev-listen-address="0.0.0.0:8200"
+(
+    # Start Vault in dev mode with the root token set as 'matt'
+    vault server -dev -dev-root-token-id="matt" -dev-listen-address="0.0.0.0:8200"
+)
 
 # It will take some time to start up, sleep for few seconds
-sleep 5s
+sleep 10s
 
 # Verify that Vault is running
 vault status
