ae05ad553ef8a0764285b160c3c84c1c9ecc3509
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2023-08-08 10:43:38 -0400

    Update install_vault.sh

--- install_vault.sh
+++ install_vault.sh
@@ -2,6 +2,20 @@
 set -e
 exec > >(tee /var/log/user-data.log) 2>&1
 
+
+# Create a configuration directory
+#sudo mkdir -p /etc/vault
+# Write the Vault configuration file
+#echo 'listener "tcp" {
+#      address     = "0.0.0.0:8200"
+#      tls_disable = 1 # Be aware, disabling TLS is not recommended for production
+#    }
+#    storage "file" {
+#      path = "/var/lib/vault/data"
+#    }' | sudo tee /etc/vault/vault.hcl
+
+
+
 # Download and install Vault
 mkdir /home/ubuntu/vault/ && cd /home/ubuntu/vault/
 sudo apt update
@@ -15,7 +29,7 @@ sudo rm -f vault_${VAULT_VERSION}_linux_amd64.zip
 export VAULT_ADDR='http://127.0.0.1:8200'
 
 # Start Vault in dev mode with the root token set as 'matt'
-vault server -dev -dev-root-token-id="matt" &
+vault server -dev -dev-root-token-id="matt" -listen-address="0.0.0.0:8200" &
 
 # It will take some time to start up, sleep for few seconds
 sleep 5s
